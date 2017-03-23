88bc1544fc0a7784ac8895cbc5cc77ef4d2360e2
klocode <kendrick.t.lo@gmail.com>
2017-03-22 22:56:00 -0400

    working on handlebar

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
-getChirp();
-function getChirp() {
-   $.getJSON('https://shielded-taiga-17184.herokuapp.com/posts')
-    .then(function(response) {
-      $('#chirpy').html('<table class="table table-striped"></table>')
-      $('#chirpy table').append('<tr><th>Avatar</th><th>User</th><th>Chirp</th></tr>')
+// Explorer
 
-      response.forEach(function(chirp){
-          $('#chirpy table').append(`
-            <tr><td><img class="img-circle" src="${chirp.user.photo_url}"></td><td> ${chirp.user.username} </td><td>  ${chirp.body}<p><small>Created At: ${moment(chirp.created_at).format('LLLL')}</small></p></td></tr>
-          `)
-        })
-      })
-}
+// getChirp();
+// function getChirp() {
+
+//
+// $.getJSON('https://shielded-taiga-17184.herokuapp.com/posts')
+// .then(function(response) {
+  // $('#chirpy').html('<table class="table table-striped"></table>')
+  // $('#chirpy table').append('<tr><th>Avatar</th><th>User</th><th>Chirp</th></tr>')
+  //
+  // response.forEach(function(chirp){
+  //     $('#chirpy table').append(`
+  //       <tr><td><img class="img-circle" src="${chirp.user.photo_url}"></td><td> ${chirp.user.username} </td><td>  ${chirp.body}<p><small>Created At: ${moment(chirp.created_at).format('LLLL')}</small></p></td></tr>
+  //     `)
+  //   })
+  // })
+// }
+
+
+$.getJSON('https://shielded-taiga-17184.herokuapp.com/posts')
+.done(function(chirps) {
+  var source   = $("#chirps-template").html();
+  var template = Handlebars.compile(source);
+  var context = {
+    user: {
+      photo_url: chirps.user.photo_url,
+      username: chirps.user.username},
+    body: chirps.body,
+    created_at: chirps.created_at};
+  var newHtml    = template(context);
+  $("#chirpy").append(newHtml)})
